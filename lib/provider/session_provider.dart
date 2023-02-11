fc66e6ba4583a02401e5110768e1fca9f99d9f7b
Aidan Dagnall <aidandagnall@gmail.com>
2023-02-11 23:53:25 +0000

    Add all required fields to provider

--- lib/provider/session_provider.dart
+++ lib/provider/session_provider.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:hangryclient/api/hangry_api.dart';
+import 'package:hangryclient/view/food_categories_page.dart';
 
 class SessionProvider extends ChangeNotifier {
   String? uuid;
   String? code;
 
+  List<Category> categories = [];
+  List<Restrictions> restrictions = [];
+  double minPrice = 10;
+  double maxPrice = 30;
+
+  bool admin = false;
+
   SessionProvider();
 
-  setUUID(String _uuid) {
+  setUUID(String _uuid, bool isAdmin) {
     uuid = _uuid;
+    admin = isAdmin;
     notifyListeners();
   }
 
   String getUUID() {
-    return uuid!;
+    return uuid == null ? "TEST" : uuid!;
   }
 
   setCode(String _code) {
@@ -21,6 +31,30 @@ class SessionProvider extends ChangeNotifier {
   }
 
   String getCode() {
-    return code!;
+    return code == null ? "TEST" : code!;
+  }
+
+  setCategories(List<Category> cats, List<Restrictions> res) {
+    categories = cats;
+    restrictions = res;
+    notifyListeners();
+  }
+
+  setPrice(double min, double max) {
+    minPrice = min;
+    maxPrice = max;
+    notifyListeners();
+  }
+
+  submitPreferences() {
+    HangryApi().postPreferences(
+      categories,
+      restrictions.contains(Restrictions.vegetarian),
+      restrictions.contains(Restrictions.alcohol),
+      minPrice,
+      maxPrice,
+      getUUID(),
+      getCode(),
+    );
   }
 }
