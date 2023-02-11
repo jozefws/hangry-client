b908a411f28d30079698b1602e57d2a10b4b7194
Aidan Dagnall <aidandagnall@gmail.com>
2023-02-11 23:54:28 +0000

    Remove Scaffold from homepage

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -2,66 +2,62 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key, required this.onNext, required this.onBack});
+  void Function() onNext;
+  void Function() onBack;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(Icons.arrow_back_ios),
-      ),
-      body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-            Container(
-              height: 200,
-              margin: const EdgeInsets.all(30),
-              child: Image.asset("images/Logo.png"),
-            ),
-            const SizedBox(height: 40),
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: Text("Welcome to Hangry!",
-                  textAlign: TextAlign.center,
-                  style: Theme.of(context).textTheme.headlineSmall),
-            ),
-            const SizedBox(height: 60),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(16),
-                    child: FloatingActionButton.extended(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/join');
-                      },
-                      label: const Text(
-                        "Join",
-                        style: TextStyle(fontSize: 20),
-                      ),
+    return Center(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+          Container(
+            height: 200,
+            margin: const EdgeInsets.all(30),
+            child: Image.asset("images/Logo.png"),
+          ),
+          const SizedBox(height: 40),
+          Container(
+            margin: const EdgeInsets.all(10),
+            child: Text("Welcome to Hangry!",
+                textAlign: TextAlign.center, style: Theme.of(context).textTheme.headlineSmall),
+          ),
+          const SizedBox(height: 60),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(16),
+                  child: FloatingActionButton.extended(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/join');
+                    },
+                    label: const Text(
+                      "Join",
+                      style: TextStyle(fontSize: 20),
                     ),
                   ),
                 ),
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(16),
-                    child: FloatingActionButton.extended(
-                      onPressed: () {
-                        Navigator.pushNamed(context, '/create');
-                      },
-                      label: const Text(
-                        "Create",
-                        style: TextStyle(fontSize: 20),
-                      ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(16),
+                  child: FloatingActionButton.extended(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/create');
+                    },
+                    label: const Text(
+                      "Create",
+                      style: TextStyle(fontSize: 20),
                     ),
                   ),
                 ),
-              ],
-            ),
-          ])),
-    );
+              ),
+            ],
+          ),
+        ]));
   }
 }
