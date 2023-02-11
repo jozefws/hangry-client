763528c7013c13de7170939bb89028ee2ff6b3c3
Aidan Dagnall <aidandagnall@gmail.com>
2023-02-11 23:54:07 +0000

    Implement buttons on choice pages

--- lib/view/food_categories_page.dart
+++ lib/view/food_categories_page.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hangryclient/api/hangry_api.dart';
 import 'package:hangryclient/helpers.dart';
+import 'package:hangryclient/provider/session_provider.dart';
 import 'package:hangryclient/view/page_view_item.dart';
+import 'package:provider/provider.dart';
 
 class FoodCategoriesPage extends StatefulWidget {
+  FoodCategoriesPage({super.key, required this.onNext, required this.onBack});
+  void Function() onNext;
+  void Function() onBack;
   @override
   State<StatefulWidget> createState() => _FoodCategoriesPageState();
 }
@@ -44,71 +50,83 @@ class _FoodCategoriesPageState extends State<FoodCategoriesPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Align(
-        alignment: FractionalOffset.bottomRight,
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 40, right: 40),
-          child: MaterialButton(
-              onPressed: choices.length > 1 ? () {} : null,
-              child: const Text("Next", style: TextStyle(fontSize: 18))),
-        ),
-      ),
-      Center(
-          child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  const Text(
-                    "Let's get started! What could you go for?",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: SvgPicture.asset("images/undraw_hamburger.svg", width: 250)),
-                  Wrap(
-                    alignment: WrapAlignment.center,
-                    spacing: 8.0,
-                    runSpacing: 4,
-                    children: Category.values
-                        .map((e) => ChoiceChip(
-                            label: Text(e.toPrettyString(), style: const TextStyle(fontSize: 16)),
-                            selected: choices.contains(e),
-                            onSelected: (value) {
-                              setState(() => value ? choices.add(e) : choices.remove(e));
-                            }))
-                        .toList(),
-                  ),
-                  AnimatedOpacity(
-                      opacity: choices.length > 1 ? 0.0 : 0.8,
-                      duration: const Duration(milliseconds: 300),
-                      child: const Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Text("Select at least 2 categories"))),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Wrap(
-                    alignment: WrapAlignment.center,
-                    spacing: 8.0,
-                    runSpacing: 4,
-                    children: Restrictions.values
-                        .map((e) => ChoiceChip(
-                            label: Text(e.toPrettyString(), style: const TextStyle(fontSize: 16)),
-                            selected: restrictions.contains(e),
-                            onSelected: (value) {
-                              setState(() => value ? restrictions.add(e) : restrictions.remove(e));
-                            }))
-                        .toList(),
-                  )
-                ],
-              )))
-    ]);
+    return Consumer<SessionProvider>(
+        builder: (context, session, child) => Stack(children: [
+              Align(
+                alignment: FractionalOffset.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 40, right: 40),
+                  child: MaterialButton(
+                      onPressed: choices.length > 1
+                          ? () {
+                              session.setCategories(choices, restrictions);
+                              widget.onNext();
+                            }
+                          : null,
+                      child: const Text("Next", style: TextStyle(fontSize: 18))),
+                ),
+              ),
+              Center(
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          const Text(
+                            "Let's get started! What could you go for?",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Padding(
+                              padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 0),
+                              child: SvgPicture.asset("images/undraw_hamburger.svg", width: 250)),
+                          Wrap(
+                            alignment: WrapAlignment.center,
+                            spacing: 8.0,
+                            runSpacing: 4,
+                            children: Category.values
+                                .map((e) => ChoiceChip(
+                                    label: Text(e.toPrettyString(),
+                                        style: const TextStyle(fontSize: 16)),
+                                    selected: choices.contains(e),
+                                    onSelected: (value) {
+                                      setState(() => value ? choices.add(e) : choices.remove(e));
+                                    }))
+                                .toList(),
+                          ),
+                          AnimatedOpacity(
+                              opacity: choices.length > 1 ? 0.0 : 0.8,
+                              duration: const Duration(milliseconds: 300),
+                              child: const Padding(
+                                  padding: EdgeInsets.all(10),
+                                  child: Text("Select at least 2 categories"))),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Wrap(
+                            alignment: WrapAlignment.center,
+                            spacing: 8.0,
+                            runSpacing: 4,
+                            children: Restrictions.values
+                                .map((e) => ChoiceChip(
+                                    label: Text(e.toPrettyString(),
+                                        style: const TextStyle(fontSize: 16)),
+                                    selected: restrictions.contains(e),
+                                    onSelected: (value) {
+                                      setState(() =>
+                                          value ? restrictions.add(e) : restrictions.remove(e));
+                                    }))
+                                .toList(),
+                          ),
+                          SizedBox(
+                            height: 100,
+                          )
+                        ],
+                      )))
+            ]));
   }
 }
--- lib/view/price_page.dart
+++ lib/view/price_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:hangryclient/api/hangry_api.dart';
+import 'package:hangryclient/provider/session_provider.dart';
+import 'package:hangryclient/view/food_categories_page.dart';
+import 'package:provider/provider.dart';
 
 class PricePage extends StatefulWidget {
+  PricePage({super.key, required this.onNext, required this.onBack});
+  void Function() onNext;
+  void Function() onBack;
+
   @override
   State<StatefulWidget> createState() => _PricePageState();
 }
@@ -11,50 +19,59 @@ class _PricePageState extends State<PricePage> {
   double top = 30.0;
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Align(
-        alignment: FractionalOffset.bottomRight,
-        child: Padding(
-          padding: const EdgeInsets.only(bottom: 40, right: 40),
-          child: MaterialButton(
-              onPressed: () {}, child: const Text("Next", style: TextStyle(fontSize: 18))),
-        ),
-      ),
-      Center(
-          child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  const Text(
-                    "Great! Now, how much do you want to spend?",
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: SvgPicture.asset("images/undraw_invoice.svg", width: 300)),
-                  const SizedBox(
-                    height: 40,
-                  ),
-                  RangeSlider(
-                    values: RangeValues(bottom, top),
-                    onChanged: (value) => setState(() {
-                      bottom = value.start;
-                      top = value.end;
-                    }),
-                    min: 1,
-                    max: 50,
-                    divisions: 5,
-                    labels:
-                        RangeLabels("£${bottom.toInt()}", top == 50 ? "£50+" : "£${top.toInt()}"),
-                  )
-                ],
-              )))
-    ]);
+    return Consumer<SessionProvider>(
+        builder: (context, session, child) => Stack(children: [
+              Align(
+                alignment: FractionalOffset.bottomRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 40, right: 40),
+                  child: MaterialButton(
+                      onPressed: () {
+                        session.setPrice(bottom, top);
+
+                        widget.onNext();
+                      },
+                      child: const Text("Next", style: TextStyle(fontSize: 18))),
+                ),
+              ),
+              Center(
+                  child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          const Text(
+                            "Great! Now, how much do you want to spend?",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          Padding(
+                              padding: const EdgeInsets.all(20),
+                              child: SvgPicture.asset("images/undraw_invoice.svg", width: 300)),
+                          const SizedBox(
+                            height: 40,
+                          ),
+                          RangeSlider(
+                            values: RangeValues(bottom, top),
+                            onChanged: (value) => setState(() {
+                              bottom = value.start;
+                              top = value.end;
+                            }),
+                            min: 1,
+                            max: 50,
+                            divisions: 5,
+                            labels: RangeLabels(
+                                "£${bottom.toInt()}", top == 50 ? "£50+" : "£${top.toInt()}"),
+                          ),
+                          const SizedBox(
+                            height: 150,
+                          )
+                        ],
+                      )))
+            ]));
   }
 }
