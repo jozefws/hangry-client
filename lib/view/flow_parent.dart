31f59b9cede0207c59cc6507a026bcde3ebdde5e
Aidan Dagnall <aidandagnall@gmail.com>
2023-02-11 23:53:38 +0000

    Add controller to parent

--- lib/view/flow_parent.dart
+++ lib/view/flow_parent.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hangryclient/model/place.dart';
 import 'package:hangryclient/view/home.dart';
 import 'package:hangryclient/view/in_out.dart';
 import 'package:hangryclient/view/food_categories_page.dart';
@@ -18,17 +19,73 @@ enum UserType { host, guest }
 class _FlowParentState extends State<FlowParent> {
   UserType? type;
 
+  final PageController _controller = PageController(initialPage: 0, keepPage: false);
+  List<Widget> children = [];
+
+  void goToNextPage() {
+    _controller.nextPage(duration: const Duration(milliseconds: 300), curve: Curves.ease);
+
+    setState(() {});
+  }
+
+  void goToLastPage() {
+    _controller.animateToPage(_controller.page!.toInt() - 1,
+        duration: const Duration(milliseconds: 300), curve: Curves.ease);
+    setState(() {
+      children.removeLast();
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    children = [
+      Home(
+        onNext: () => goToNextPage(),
+        onBack: () => goToLastPage(),
+      )
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: _controller.positions.isEmpty ? false : _controller.page! != 0,
+      ),
       body: PageView(
+        controller: _controller,
+        // physics: const NeverScrollableScrollPhysics(),
         children: [
-          Home(),
-          ModeSelector(),
-          FoodCategoriesPage(),
-          PricePage(),
-          WaitingPage(),
-          RestaurantChoicePage()
+          Home(
+            onNext: () => goToNextPage(),
+            onBack: () {},
+          ),
+          ModeSelector(
+            onNext: () => goToNextPage(),
+            onBack: () => goToLastPage(),
+          ),
+          FoodCategoriesPage(
+            onNext: () => goToNextPage(),
+            onBack: () => goToLastPage(),
+          ),
+          PricePage(
+            onNext: () => goToNextPage(),
+            onBack: () => goToLastPage(),
+          ),
+          const WaitingPage(),
+          RestaurantChoicePage(choices: [
+            const Place(
+                id: "dlsfjsldkfj",
+                name: "Restaurant 1",
+                description: "This is a very good restaurant",
+                photo: ""),
+            const Place(
+                id: "dlsfjsldkfj",
+                name: "Restaurant 2",
+                description: "This is a very good restaurant",
+                photo: ""),
+          ])
         ],
       ),
     );
