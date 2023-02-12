8d44d8d77c4a458178d11d0560e387248db86545
Aidan Dagnall <aidandagnall@gmail.com>
2023-02-12 00:17:13 +0000

    Use provider to check if admin

--- lib/view/results.dart
+++ lib/view/results.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:hangryclient/provider/session_provider.dart';
+import 'package:provider/provider.dart';
 
 import '../model/user.dart';
 
@@ -13,61 +15,61 @@ class WaitingPage extends StatefulWidget {
 class _WaitingPageState extends State<WaitingPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-        child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.max,
-      children: [
-        Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40),
-            child: SvgPicture.asset(
-              "images/undraw_wait_in_line.svg",
-              height: 140,
-            )),
-        const Padding(
-            padding: EdgeInsets.all(20), child: CircularProgressIndicator()),
-        const Padding(
-            padding: EdgeInsets.only(top: 40),
-            child: Text(
-              "Waiting on your friends",
-              textAlign: TextAlign.center,
-              style: TextStyle(fontSize: 30),
-            )),
-        const Text(
-          "Give them a minute...",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 20),
-        ),
-        const SizedBox(
-          height: 40,
-        ),
-        Container(height: 100, child: SelfieDisplayGen()),
-        const SizedBox(
-          height: 40,
-        ),
-        if (user.isHost)
-          Container(
-            width: 250,
-            height: 120,
-            margin: const EdgeInsets.all(10),
-            child: Expanded(
-              child: FloatingActionButton.extended(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/order');
-                },
-                label: const Text(
-                  "Order",
+    return Consumer<SessionProvider>(
+        builder: (context, session, child) => Center(
+                child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 40),
+                    child: SvgPicture.asset(
+                      "images/undraw_wait_in_line.svg",
+                      height: 140,
+                    )),
+                const Padding(padding: EdgeInsets.all(20), child: CircularProgressIndicator()),
+                const Padding(
+                    padding: EdgeInsets.only(top: 40),
+                    child: Text(
+                      "Waiting on your friends",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 30),
+                    )),
+                const Text(
+                  "Give them a minute...",
+                  textAlign: TextAlign.center,
                   style: TextStyle(fontSize: 20),
                 ),
-                icon: SvgPicture.asset("images/orderIn.svg", width: 120),
-              ),
-            ),
-          ),
-        const SizedBox(
-          height: 80,
-        )
-      ],
-    ));
+                const SizedBox(
+                  height: 40,
+                ),
+                Container(height: 100, child: SelfieDisplayGen()),
+                const SizedBox(
+                  height: 40,
+                ),
+                if (session.admin)
+                  Container(
+                    width: 250,
+                    height: 120,
+                    margin: const EdgeInsets.all(10),
+                    child: Expanded(
+                      child: FloatingActionButton.extended(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/order');
+                        },
+                        label: const Text(
+                          "Order",
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        icon: SvgPicture.asset("images/orderIn.svg", width: 120),
+                      ),
+                    ),
+                  ),
+                const SizedBox(
+                  height: 80,
+                )
+              ],
+            )));
   }
 }
 
