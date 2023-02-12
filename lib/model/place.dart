029d7c9141fc5c871bcc53106c26b172130d9389
Joe <joe@sieniawski.co.uk>
2023-02-12 04:36:42 +0000

    Results done idk :sob: :sleepy: :zzz:

--- lib/model/place.dart
+++ lib/model/place.dart
@@ -1,14 +1,42 @@
+import 'package:tuple/tuple.dart';
+
 class Place {
-  const Place(
-      {required this.id, required this.name, required this.description, required this.photo});
+  const Place({
+      required this.id,
+      required this.name,
+      required this.description,
+      required this.photos,
+      required this.healthRating,
+      required this.googleRating,
+      required this.location,
+      required this.wheelchairAccessible,
+      required this.priceRange,
+      required this.matchScore
+  });
 
   final String id;
   final String name;
   final String description;
-  final String photo;
+  final List<String> photos;
+  final String healthRating;
+  final String googleRating;
+  final Tuple2<double, double> location;
+  final bool wheelchairAccessible;
+  final String priceRange;
+  final double matchScore;
 
   factory Place.fromJson(Map<String, dynamic> json) {
     return Place(
-        id: json['id'], name: json['name'], description: json['description'], photo: json['photo']);
+        id: json['id'],
+        name: json['name'],
+        description: json['description'],
+        photos: json['photo'],
+        healthRating: json['healthRating'],
+        googleRating: json['googleRating'],
+        location: Tuple2(json['location'][0], json['location'][1]),
+        wheelchairAccessible: json['wheelchairAccessible'],
+        priceRange: json['priceRange'],
+        matchScore: json['matchScore']
+    );
   }
 }
